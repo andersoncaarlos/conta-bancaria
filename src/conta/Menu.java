2a1cdb2b57b19105a726716e06753b290f33c88b
Anderson <ander_son12@hotmail.com>
2023-12-19 21:51:07 -0300

    Transformando Classe Conta em Abstrata, Criando Interface ContaRepository e Implementando Método KeyPress na Classe Menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,7 +1,11 @@
 package conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 import java.util.Scanner;
 
@@ -10,9 +14,25 @@ public class Menu {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);		
+		Scanner sc = new Scanner(System.in);				
 		
-		int opcao;
+		int opcao;		
+		
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+        // Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		while (true) {
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
@@ -36,7 +56,13 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                            ");
 			System.out.println("                                                       " + Cores.TEXT_RESET);
 			
+			try {
 			opcao = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				sc.nextLine();
+				opcao = 0;
+			}
 			
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu futuro começa aqui! ");
@@ -47,39 +73,49 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta\n\n");
+				keyPress();
 
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas\n\n");
-
+				keyPress();
+				
 				break;
 			case 3:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta - por número\n\n");
-
+				keyPress();
+				
 				break;
 			case 4:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
-
+				keyPress();
+				
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
-
+				keyPress();
+				
 				break;
 			case 6:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
-
+				keyPress();
+				
 				break;
 			case 7:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
-
+				keyPress();
+				
 				break;
 			case 8:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
-
+				keyPress();
+				
 				break;
 			default:
 				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
 				System.out.println(Cores.TEXT_RESET);
+				keyPress();
+				
 				break;
 			
 			}
@@ -93,6 +129,18 @@ public class Menu {
 		System.out.println("https://github.com/andersoncaarlos/contabancaria");
 		System.out.println("***********************************************************");
 		
+	}
+	
+	public static void keyPress() {
+		try {
+			
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+			
+		} catch (IOException e) {
+			
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+		}
 	}
 
 }
